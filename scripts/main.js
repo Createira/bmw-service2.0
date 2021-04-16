2b34e61e0c98f805f657e2888d0836f3294c834d
Irina <ira.verbilovich@mail.ru>
2021-04-16 22:37:51 +0300

    added fixed header

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,17 @@ window.onload = function() {
          preloader.classList.add('preloader-hidden');
    }, 1700);
 }
+
+// Fixed Header
+const header = document.querySelector('.header'); //Find out the element header 
+const headerHeight = header.offsetHeight; //Find out the height of the element
+
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+
+	if (scrollDistance >= headerHeight) {
+		header.classList.add('header--fixed');
+	} else {
+		header.classList.remove('header--fixed');
+	}
+});
