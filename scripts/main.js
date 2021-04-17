ae77c1ef6d62847c03ba931ba9f0319e1de63334
Irina <ira.verbilovich@mail.ru>
2021-04-17 23:25:40 +0300

    divided js scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
-//Preloader
-window.onload = function() {
-   let preloader = document.getElementById('loader-wrapper');
-   preloader.classList.add('hide-preloader');
-   setInterval(function() {
-         preloader.classList.add('preloader-hidden');
-   }, 1700);
-}
-
 // Fixed Header
 const header = document.querySelector('.header'); //Find out the element header 
 const headerHeight = header.offsetHeight; //Find out the height of the element
