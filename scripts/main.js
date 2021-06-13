a6aba4a8ab9afb8f7e00d57ad61fd7420850ac96
Irina <ira.verbilovich@mail.ru>
2021-06-13 14:01:35 +0300

    fixed bugs on mobile version

--- scripts/main.js
+++ scripts/main.js
@@ -5,24 +5,3 @@ $("#nav_toggle").on("click", function(event) {
   
       $("#nav").toggleClass("active");
 }); 
-
-// Инициализая секции с которой работаем
-let grid = new Isotope('.grid', {
-      itemSelector: '.grid-item',
-});
-  
-// Работаем с кнопками фильтров
-let filterBtn = document.querySelectorAll('.filters-button .filter-btn');
-for (let i = 0; i < filterBtn.length; i++) {
-// Если кликнули по ссылке
-filterBtn[i].onclick = function (click) {
-       // Отменяем переход
-      click.preventDefault();
-      // Получаем значение дата-атрибута кнопки
-      let filterData = event.target.getAttribute('data-filter');
-      // Применяем фильтрацию элементов в Isotope
-      grid.arrange({
-            filter: filterData
-      });
-};
-}
