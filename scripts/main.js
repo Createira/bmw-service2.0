c0bd7609f2f02b29430ebee543b83a0180650ba5
Irina <ira.verbilovich@mail.ru>
2021-06-13 14:54:10 +0300

    added button to top

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,17 @@ $("#nav_toggle").on("click", function(event) {
   
       $("#nav").toggleClass("active");
 }); 
+
+//Scroll btn
+let btn = document.querySelector('.btn__to-top')
+function magic() {
+  if (window.pageYOffset > 300) {
+    btn.style.opacity = '1'
+  } else { btn.style.opacity = '0' }
+}
+//
+btn.onclick = function () {
+	window.scrollTo(0,0)
+}
+
+window.onscroll = magic
