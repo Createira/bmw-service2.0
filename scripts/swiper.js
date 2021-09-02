6d1d07f4d87a191c8a12ebf33aa0e436d7fd987b
Irina <ira.verbilovich@mail.ru>
2021-09-02 11:57:55 +0300

    fixed bugs in first swiper slider

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,39 +1,11 @@
-// Initialize Swiper
-var swiper = new Swiper('.swiper-container-one', {
-   spaceBetween: 30,
-   centeredSlides: true,
-   autoplay: {
-     delay: 5000,
-     disableOnInteraction: false,
-   },
-   pagination: {
-     el: '.swiper-pagination',
-     clickable: true,
-   },
-});
-
-// Initialize Swiper
-var swiper = new Swiper('.swiper-container-two', {
-  slidesPerView: 3,
-  slidesPerColumn: 1,
-  spaceBetween: 30,
+var swiper = new Swiper(".myswiper", {
+  loop: true,
   autoplay: {
-    delay: 5000,
+    delay: 2500,
     disableOnInteraction: false,
   },
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    500: {
-      slidesPerView: 2,
-    },
-    767: {
-      slidesPerView: 3,
-    },
+    el: ".swiper-pagination",
+    dynamicBullets: true,
   },
 });
